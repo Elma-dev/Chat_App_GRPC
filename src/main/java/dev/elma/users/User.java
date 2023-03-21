a256c8bdcf7c3953ea4f6f9915a5ba9f3c8e5d6e
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-03-21 18:21:29 +0000

    commit

--- src/main/java/dev/elma/users/User.java
+++ src/main/java/dev/elma/users/User.java
@@ -6,10 +6,11 @@ import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class User {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ManagedChannel localhost = ManagedChannelBuilder.forAddress("localhost", 2001).usePlaintext().build();
         chatGrpc.chatStub chatStub = chatGrpc.newStub(localhost);
 
@@ -20,12 +21,16 @@ public class User {
         System.out.print("Username : ");
         username=scanner.next();
         System.out.println(username+ " connected...");
-        System.out.println("To disconnect write (quit) in message...");
+        System.out.println("To disconnect write (disc) in message...");
 
-       /* StreamObserver<Chat.request> send = chatStub.send(new StreamObserver<Chat.request>() {
+
+
+       StreamObserver<Chat.request> send = chatStub.send(new StreamObserver<Chat.request>() {
             @Override
             public void onNext(Chat.request request) {
-
+                String messageFrom = request.getMessageFrom();
+                String content = request.getContent();
+                System.out.println(messageFrom+" : "+content);
             }
 
             @Override
@@ -39,7 +44,39 @@ public class User {
             }
         });
 
-        */
+       while (true){
+           System.out.println("---------------------------------------------");
+           System.out.print("Message To : ");
+           String messageTo=scanner.next();
+           System.out.print("Message : ");
+           message=scanner.next();
+           System.out.println(message);
+           if(message.equals("disc")) {
+               Chat.connect disconnect = Chat.connect.newBuilder().setUsername(username).build();
+               chatStub.disconnectReq(disconnect, new StreamObserver<Chat.request>() {
+                   @Override
+                   public void onNext(Chat.request request) {
+                       System.out.println(request.getContent());
+                   }
+
+                   @Override
+                   public void onError(Throwable throwable) {
+
+                   }
+
+                   @Override
+                   public void onCompleted() {
+
+                   }
+               });
+               //System.in.read();
+               break;
+           }
+           Chat.request request = Chat.request.newBuilder().setMessageFrom(username).setMessageTo(messageTo).setContent(message).build();
+           send.onNext(request);
+       }
+
+
 
 
 
